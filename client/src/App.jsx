56fb5a8253fe2e1d75a59ca497241b9c6d02e243
Anjali-kumari-0 <anjali.kumari@mayadataprivacy.eu>
2026-02-14 17:33:48 +0530

    fixed file name bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
-import ChatPage from "./pages/ChatPage";
+import ChatPage from "./pages/Chatpage";
 import ProfileLandingPage from "./pages/ProfileLandingPage";
 import Notification from "./components/Notification";
 import "./App.css";
